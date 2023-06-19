4a8709258dbb7b81ed98bb72e490a8e84b905c41
neeru0713 <neerurani1307@gmail.com>
2023-06-19 20:49:31 +0530

    landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import defaultImage from './images/logo.jpeg';
+import biz from './images/biz.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-100">
+
+      <div className="row navbar d-flex">
+        <div className="logo h-100 d-flex align-items-center fw-medium">
+          <img src={defaultImage} className="img-fluid h-100 "/> 
+          <p className="m-1">Webure</p>
+        </div>
+
+        <div className="nav-btns d-flex align-items-center fw-medium justify-content-between">
+          <div className="">Home</div>
+          <div className="">About Us</div>
+          <div className="">Services</div>
+          <div className="">Career</div>
+          <div className="">Clientele</div>
+        </div>
+        
+        <div className="contactus-btn">
+          <button className="rounded-pill p-2">Contact Us 
+
+          </button>
+        </div>
+
+      </div>
+
+      <div className="row landingpage d-flex flex-column align-items-center">
+        <div className="landingpage-container d-flex flex-column align-items-center">
+          <div className="content text-center">
+            <h1>We esure and transfeorm business growing</h1>
+          </div>
+          <div className="text text-center">
+              <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
+          </div>
+          <div className="btn-pills d-flex justify-content-between" >
+              <button className=" getstarted rounded-pill px-3 py-2">Get Started </button>
+              <button className=" learnmore rounded-pill px-3 py-2">Learn More </button>
+          </div>
+        </div>
+        <div className="image1 mt-5" style={{height: '50%', maxWidth: '40%' }}>
+          <img src={biz} className="h-100 w-100"/>
+        </div>
+      </div>
     </div>
   );
 }
